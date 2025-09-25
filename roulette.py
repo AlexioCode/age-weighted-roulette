40fc95f75a261911223f5d95ef6ffa8d95527c8e
Alejandro Alonso-Basurto <alexalonsobasurtodiaz@gmail.com>
2025-09-25 13:36:27 +0200

    variable translation to english

--- roulette.py
+++ roulette.py
@@ -5,62 +5,62 @@ oneYearAddsXProbability = 20
 ageMultiplier = oneYearAddsXProbability / 365
 
 class Element:
-    def __init__(self, nombre, fecha, prob = 0.0):
-        self.nombre = nombre
-        self.fecha = fecha
+    def __init__(self, name, date, prob = 0.0):
+        self.name = name
+        self.date = date
         self.prob = prob
 
-# Asignar probabilidades a los juegos
-def asignarProbabilidadesAJuegos(listaJuegos: list):
-    indiceJuego = 0
+# Asignar probabilidades a los juegos y devuelve el índice del juego que más probabilidad tiene
+def assignProbabilitesToElements(elementsList: list):
+    elementIndex = 0
     probMax = 0
-    for juego in listaJuegos:
-        juego.prob = random.randint(0, 100)
-        antiguedad = dt.datetime.now() - juego.fecha
-        juego.prob = juego.prob + antiguedad.days * ageMultiplier # antiguedad.days is int
-        if (juego.prob > probMax):
-            probMax = juego.prob
-            indiceJuego = listaJuegos.index(juego)
-    return indiceJuego
+    for element in elementsList:
+        element.prob = random.randint(0, 100)
+        antiguedad = dt.datetime.now() - element.date
+        element.prob = element.prob + antiguedad.days * ageMultiplier # antiguedad.days is int
+        if (element.prob > probMax):
+            probMax = element.prob
+            elementIndex = elementsList.index(element)
+    return elementIndex
 
 
 if __name__ == '__main__':
 
-    cadJuego = ""
-    listaJuegos = []
+    elementStr = ""
+    elementsList = []
 
     # Introducir juegos ($ para terminar)
-    while (cadJuego != "$"):
-        cadJuego = input("Introduce juego y fecha en la que lo apuntaste en la lista (e.g. Stendhal, 29/02/2023) (no es obligatorio introducir fecha) [$ para terminar]: \n")
-        if (cadJuego != "$"): # Si no se ha introducido '$'
-            if(cadJuego[0] == "-"): # Eliminar guión del principio
-                cadJuego = cadJuego[1:]
-            if(cadJuego[0] == " "): # Eliminar espacio del principio
-                cadJuego = cadJuego[1:]
-            if(cadJuego[0].islower()): # Si la primera letra es minúscula, poner en mayúscula
-                cadJuego = cadJuego[0].upper() + cadJuego[1:]
-            
-            nombreJuego = cadJuego.split(",")[0]
-            if (len(cadJuego.split(",")) > 1): # Si fecha está especificada
-                cadFecha = cadJuego.split(",")[1]
-                if(cadFecha[0] == " "): # Eliminar espacio después de la coma
-                    cadFecha = cadFecha[1:]
+    while (elementStr != "$"):
+        elementStr = input("Introduce juego y fecha en la que lo apuntaste en la lista (e.g. Stendhal, 29/02/2023) (no es obligatorio introducir fecha) [$ para terminar]: \n")
+        if (elementStr != "$"): # Si no se ha introducido '$'
+            if(elementStr[0] == "-"): # Eliminar guión del principio
+                elementStr = elementStr[1:]
+            if(elementStr[0] == " "): # Eliminar espacio del principio
+                elementStr = elementStr[1:]
+            if(elementStr[0].islower()): # Si la primera letra es minúscula, poner en mayúscula
+                elementStr = elementStr[0].upper() + elementStr[1:]
+            elementName = elementStr.split(",")[0]
+
+            if (len(elementStr.split(",")) > 1): # Si fecha está especificada
+                dateStr = elementStr.split(",")[1]
+                if(dateStr[0] == " "): # Eliminar espacio después de la coma
+                    dateStr = dateStr[1:]
             else: # Si no indicamos fecha, se le asigna la actual
-                cadFecha = dt.datetime.now().strftime("%d/%m/%Y")
+                dateStr = dt.datetime.now().strftime("%d/%m/%Y")
                 
             # Construir el juego y añadirlo a la lista
-            listaJuegos.append(Element(nombreJuego, dt.datetime.strptime(cadFecha, "%d/%m/%Y")))
+            elementsList.append(Element(elementName, dt.datetime.strptime(dateStr, "%d/%m/%Y")))
 
-    print("\n\nTienes " + str(len(listaJuegos)) + " juegos pendientes.")
+    print("\n\nTienes " + str(len(elementsList)) + " juegos pendientes.")
     print("Lista de juegos pendientes:")
-    for juego in listaJuegos:
-        print(juego.nombre)
+    for element in elementsList:
+        print(element.name)
 
-    repetir = 's'
-    while repetir == 's' and listaJuegos: # mientras selecciones que sí y haya elementos en la lista
+    repeat = 's'
+    while repeat == 's' and elementsList: # mientras selecciones que sí y haya elementos en la lista
         print("\nTirando de la ruleta...")
-        indiceJuego = asignarProbabilidadesAJuegos(listaJuegos)
-        print("\n\nTE HA TOCADO JUGAR: " + listaJuegos[indiceJuego].nombre + "\ncon una probabilidad de " + str(listaJuegos[indiceJuego].prob))
-        listaJuegos.pop(indiceJuego)
-        if listaJuegos:
-            repetir = input("¿Quieres volver a tirar de la ruleta? (s/n): ")
+        elementIndex = assignProbabilitesToElements(elementsList)
+        print("\n\nTE HA TOCADO JUGAR: " + elementsList[elementIndex].name + "\ncon una probabilidad de " + str(elementsList[elementIndex].prob))
+        elementsList.pop(elementIndex)
+        if elementsList:
+            repeat = input("¿Quieres volver a tirar de la ruleta? (s/n): ")
