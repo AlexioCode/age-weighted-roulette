74b900d322d8e188700b38eb7e5499b6118106e8
Alejandro Alonso-Basurto <alexalonsobasurtodiaz@gmail.com>
2025-09-25 13:45:08 +0200

    empty element case treatment added

--- roulette.py
+++ roulette.py
@@ -32,24 +32,26 @@ if __name__ == '__main__':
     # Introducir juegos ($ para terminar)
     while (elementStr != "$"):
         elementStr = input("Introduce juego y fecha en la que lo apuntaste en la lista (e.g. Stendhal, 29/02/2023) (no es obligatorio introducir fecha) [$ para terminar]: \n")
-        if (elementStr != "$"): # Si no se ha introducido '$'
-            if(elementStr[0] == "-"): # Eliminar guión del principio
-                elementStr = elementStr[1:]
-            if(elementStr[0] == " "): # Eliminar espacio del principio
-                elementStr = elementStr[1:]
-            if(elementStr[0].islower()): # Si la primera letra es minúscula, poner en mayúscula
-                elementStr = elementStr[0].upper() + elementStr[1:]
-            elementName = elementStr.split(",")[0]
+        
+        if elementStr != "": # Si no se introduce nada, vuelve a pedir que se introduzca
+            if (elementStr != "$"): # Si no se ha introducido '$'
+                if(elementStr[0] == "-"): # Eliminar guión del principio
+                    elementStr = elementStr[1:]
+                if(elementStr[0] == " "): # Eliminar espacio del principio
+                    elementStr = elementStr[1:]
+                if(elementStr[0].islower()): # Si la primera letra es minúscula, poner en mayúscula
+                    elementStr = elementStr[0].upper() + elementStr[1:]
+                elementName = elementStr.split(",")[0]
 
-            if (len(elementStr.split(",")) > 1): # Si fecha está especificada
-                dateStr = elementStr.split(",")[1]
-                if(dateStr[0] == " "): # Eliminar espacio después de la coma
-                    dateStr = dateStr[1:]
-            else: # Si no indicamos fecha, se le asigna la actual
-                dateStr = dt.datetime.now().strftime("%d/%m/%Y")
-                
-            # Construir el juego y añadirlo a la lista
-            elementsList.append(Element(elementName, dt.datetime.strptime(dateStr, "%d/%m/%Y")))
+                if (len(elementStr.split(",")) > 1): # Si fecha está especificada
+                    dateStr = elementStr.split(",")[1]
+                    if(dateStr[0] == " "): # Eliminar espacio después de la coma
+                        dateStr = dateStr[1:]
+                else: # Si no indicamos fecha, se le asigna la actual
+                    dateStr = dt.datetime.now().strftime("%d/%m/%Y")
+                    
+                # Construir el juego y añadirlo a la lista
+                elementsList.append(Element(elementName, dt.datetime.strptime(dateStr, "%d/%m/%Y")))
 
     print("\n\nTienes " + str(len(elementsList)) + " juegos pendientes.")
     print("Lista de juegos pendientes:")
